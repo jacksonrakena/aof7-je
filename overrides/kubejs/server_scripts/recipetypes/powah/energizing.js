ab948a0362ccde39ac1d5ca3cf632ede5b5827cd
Jackson <jackson@rakena.co.nz>
2024-01-29 19:40:40 +1300

    add configs from aof7

---
+++ overrides/kubejs/server_scripts/recipetypes/powah/energizing.js
@@ -0,0 +1,42 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+ServerEvents.recipes(event => {
+
+  /*
+    event.custom({    
+        "type": "powah:energizing",
+        "id": "kibe:angel_ring",
+        "ingredients": [
+          {"item": "deeperdarker:soul_elytra"},
+          {"item": "spectrum:take_off_belt"},
+          {"item": "ad_astra:calorite_engine"},
+          {"item": "bosses_of_mass_destruction:ancient_anima"},
+          {"item": "piercingpaxels:upgrade_unbreakable"},
+          {"item": "botania:gaia_ingot"}
+        ],
+        "energy": "400000000",
+        "result": {
+          "item": "kibe:angel_ring"
+        }
+})
+*/
+
+  event.custom({    
+        "type": "powah:energizing",
+        "id": "aof:powah/hyper_nitro_crystal",
+        "ingredients": [
+          {"item": "powah:nitro_crystal_block"},
+          {"item": "powah:nitro_crystal_block"},
+          {"item": "powah:nitro_crystal_block"},
+          {"item": "powah:nitro_crystal_block"},
+          {"item": "powah:nitro_crystal_block"},
+          {"item": "powah:nitro_crystal_block"}
+        ],
+        "energy": "400000000",
+        "result": {
+          "item": "kubejs:hyper_nitro_crystal"
+        }
+    })
+    })
